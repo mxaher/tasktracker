92e06d69d05febe0d6ce007735da139257762c48
Mohamed Zaher <m.zaher@almarshad.com>
2026-04-26 08:20:53 +0300

    fix: remove invalid esbuildOptions from cloudflare config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,10 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
-  // Bundle Prisma WASM files properly
-  esbuildOptions: {
-    loader: {
-      ".wasm": "copy",
-    },
-  },
-});
+export default defineCloudflareConfig({});
